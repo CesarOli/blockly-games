e1a23045403503c0de6deeb37e96a9424dace673
CesarOli <cesaraleoliveira@gmail.com>
2023-09-17 23:42:54 -0300

    maze.dois.py

---
+++ maze_dois.py
@@ -0,0 +1,33 @@
+def move_forward():
+    print('move forward')
+
+def turn_left():
+    print('turn left')
+
+def turn_right():
+    print('turn right')
+
+
+# EXERCÍCIO 2 DO MAZE
+i = 0 
+for i in [1]:
+    move_forward()
+    turn_left()
+    move_forward()
+    turn_right()
+move_forward()
+
+print()
+
+# msm resultado 
+move_forward()
+turn_left()
+move_forward()
+turn_right()
+move_forward()
+
+
+print('\nFim!')
+
+
+
