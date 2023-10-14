1b793eaf2fa4eda00cc98be98e4d3f352e7e2196
CesarOli <cesaraleoliveira@gmail.com>
2023-10-14 20:59:44 -0300

    Movie um.

---
+++ movie/movie_um.py
@@ -0,0 +1,27 @@
+#movie_um
+
+#configura a cor do lapis
+def pencil_color(color):
+    print(color)
+
+#configura o desenho de um círculo
+def draw_circle(x, y, radius):
+    print(draw_circle)
+
+#configura o desenho de um retângulo
+def draw_rectangle(x, y, width, height):
+    print(draw_rectangle)
+
+#configura o desenho de uma linha
+def draw_line(x1, y1, x2, y2, width):
+    print(draw_line)
+
+
+draw_circle(50, 70, 10)
+pencil_color('black')
+draw_rectangle(50, 40, 20, 40)
+pencil_color('black')
+draw_line(60, 50, 80, 70, 5)
+pencil_color('black')
+draw_line(20, 70, 40, 50, 5)
+
