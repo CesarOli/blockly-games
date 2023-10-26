43ff01b0f1c45c861ae88fdcf8379dc31e21dcbe
CesarOli <cesaraleoliveira@gmail.com>
2023-10-26 19:47:22 -0300

    Movie seis, correção na fórmula matemática.

--- movie/movie_seis.py
+++ movie/movie_seis.py
@@ -20,10 +20,11 @@ draw_circle(50, 70, 10)
 pencil_color('blue')
 draw_rectangle(50, 40, 20, 40)
 pencil_color('blue')
-draw_line(80, (range(1, 101)-50)/5)^2
+draw_line(80, ((range(1, 101) - 50) / 5) ** 2, 60, 50, 5)
 pencil_color('blue')
 draw_line(20, 100 - range(1, 101), 40, 50, 5)
+draw_line(100 - range(1, 101), 0, 60, 20, 5)
 draw_line(range(1, 101), 0, 40, 20, 5)
-draw_line(range(1, 101, 0, 60, 20, 5))
 draw_circle(20, 100 - range(1, 101), 5)
-draw_circle(80, (range(1, 101)-50)/5)^2
+draw_circle((80, ((range(1, 101) - 50) / 5) ** 2, 5)
+            
