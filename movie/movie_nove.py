a5879a613e9b66e1f40d48551a9408b537a5739a
CesarOli <cesaraleoliveira@gmail.com>
2023-10-28 23:30:05 -0300

    Movie nove.

---
+++ movie/movie_nove.py
@@ -0,0 +1,42 @@
+#movie_nove
+
+#configura a cor do lapis
+def pencil_color(color):
+    print('color')
+
+#configura o desenho de um círculo
+def draw_circle(x, y, radius):
+    print('draw_circle')
+
+#configura o desenho de um retângulo
+def draw_rectangle(x, y, width, height):
+    print('draw_rectangle')
+
+#configura o desenho de uma linha
+def draw_line(x1, y1, x2, y2, width):
+    print('draw_line')
+
+
+pencil_color('green')
+draw_circle(50, range(1, 101) / 2, range(1, 101) / 2)
+pencil_color('red')
+if ((range(1, 101) > 50)):
+    draw_circle(50, 80, 20)
+else:
+    draw_circle(50, 70, 10)
+
+pencil_color('blue')
+draw_rectangle(50, 40, 20, 40)
+pencil_color('black')
+draw_line(20, 100 - range(1, 101), 40, 50, 5)
+draw_line(80, ((range(1, 101) - 50) / 5) ** 2, 60, 50, 5)
+
+if ((range(1, 101) > 50)):
+    draw_line(range(1, 101),0, 60, 20, 5)
+    draw_line(100,(range(1, 101)), 0, 40, 20, 5)
+else:
+    draw_line(100 - range(1, 101), 0, 60, 20, 5)
+    draw_line(range(1, 101), 0, 40, 20, 5)
+
+draw_circle(20, 100 - range(1, 101), 5)
+draw_circle(80, ((range(1, 101) - 50) / 5) ** 2, 60, 50, 5)
