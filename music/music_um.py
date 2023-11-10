197566e64ec88a44a7433cb64b2b692eb43f3ad0
CesarOli <cesaraleoliveira@gmail.com>
2023-11-10 15:03:05 -0300

    Musica um.

---
+++ music/music_um.py
@@ -0,0 +1,9 @@
+def play(duration, pitch):
+    print(f'Play note with duration {duration} and pitch {pitch}')
+
+
+def start():
+    play(0.25, '7')
+    play(0.25, '8')
+    play(0.25, '9')
+    play(0.25, '7')
